839151b86af6aa0b5e42b9527f44b2e5ba75bf7e
白茶 <5483376+vaemath@user.noreply.gitee.com>
2023-07-12 16:36:57 +0800

    add print_main2

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ void print_main(){
     std::cout << "this is main" << std::endl;
 }
 
+void print_main2(){
+    std::cout << "this is main2" << std::endl;
+}
+
 int main() {
     std::cout << "this is feature_01" << std::endl;
     return 0;
