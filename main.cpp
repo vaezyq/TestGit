fc402283a4eca4cc7207d7acbef990b5cb319547
白茶 <5483376+vaemath@user.noreply.gitee.com>
2023-07-12 16:34:48 +0800

    add print_main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ void print_a() {
     std::cout << "this id A" << std::endl;
 }
 
+void print_main(){
+    std::cout << "this is main" << std::endl;
+}
 
 int main() {
     std::cout << "this is feature_01" << std::endl;
